ac61690ea83ae2c670b635ccce23c25a8bccaf3f
Felipe N. Moura <felipenmoura@gmail.com>
2016-05-21 21:15:32 -0700

    Redirecting image

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
 self.addEventListener('fetch', (event)=>{
     console.log('FETCHING: ', event.request.url);
-    event.respondWith(
-        new Response('Yeah baby, yeah!');
-    );
-    debugger;
+    
+    const url = new URL(event.request.url);
+    
+    if (url.pathname.match(/^\/images\/public\//)) {
+        event.respondWith(
+            fetch('/images/public/default.png')
+        );
+    }
+    
+//    event.respondWith(
+//        new Response('Yeah baby, yeah!');
+//    );
+    //debugger;
 });
