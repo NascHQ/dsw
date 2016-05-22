b5e3efa815bc1c170c3a717deed6b582c05c212c
Felipe N. Moura <felipenmoura@gmail.com>
2016-05-21 21:50:32 -0700

    treating not found requisitions

--- sw.js
+++ sw.js
@@ -3,14 +3,32 @@ self.addEventListener('fetch', (event)=>{
     
     const url = new URL(event.request.url);
     
-    if (url.pathname.match(/^\/images\/public\//)) {
-        event.respondWith(
-            fetch('/images/public/default.png')
-        );
-    }
+//    if (url.pathname.match(/^\/images\/public\//)) {
+//        event.respondWith(
+//            fetch('/images/public/default.png')
+//        );
+//    }
     
-//    event.respondWith(
-//        new Response('Yeah baby, yeah!');
-//    );
+    event.respondWith(
+        fetch(event.request).then(response=>{
+            // will fetch exactly what was requested
+            // but will be able to do something after the fetch
+            // and before returning it
+            if (response.status === 404) {
+                // if it was not found
+                var url = new URL(response.url);
+                
+                if (url.pathname.startsWith('/images/')) {
+                    // in case it is an image, we deliver the default image
+                    return fetch('/images/public/404.jpg');
+                }else{
+                    // otherwise, we simply return our 404 page
+                    return fetch('/404.html');
+                }
+            }
+            
+            return response;
+        })
+    );
     //debugger;
 });
