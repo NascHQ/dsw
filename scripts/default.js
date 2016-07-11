e76370d8e15e05c598cdd091827f312955c72d20
Felipe N. Moura <felipenmoura@gmail.com>
2016-07-11 11:09:33 -0300

    Updates

---
+++ scripts/default.js
@@ -0,0 +1,62 @@
+console.log('LOADED THE SCRIPT');
+
+window.addEventListener('load', function(){
+    
+    function set (el, attr, src) {
+        el[attr] = '';
+        el[attr] = src;
+    }
+    
+    document.getElementById('btn-img-1').addEventListener('click', function(){
+        set(document.getElementById('test-1-image'), 'src', 'images/public/gears.png');
+    });
+    
+    document.getElementById('btn-img-2').addEventListener('click', function(){
+        set(document.getElementById('test-2-image'), 'src', 'images/public/something.png');
+    });
+    
+    document.getElementById('btn-img-3').addEventListener('click', function(){
+        set(document.getElementById('test-3-image'), 'src', 'images/legacy-images/foo.png');
+    });
+    
+    document.getElementById('btn-img-4').addEventListener('click', function(){
+        set(document.getElementById('test-4-image'), 'src', 'images/not-cached.jpg');
+    });
+    
+    document.getElementById('btn-5-page').addEventListener('click', function(){
+        set(document.getElementById('test-5-iframe'), 'src', '/foo.html');
+    });
+    document.getElementById('btn-6-data').addEventListener('click', function(){
+        set(document.getElementById('test-6-iframe'), 'src', '/api/user/1.json');
+    });
+    document.getElementById('btn-7-page').addEventListener('click', function(){
+        let listOfOlderPages = [
+            'index.html',
+            'page-1.html',
+            'about.html',
+            'articles.html',
+            'contact.html'
+        ];
+        let idx = Math.ceil(Math.random() * 5) -1;
+        console.log(idx);
+        set(document.getElementById('test-7-iframe'), 'src', '/old-site/' +
+            listOfOlderPages[idx]);
+    });
+    /*
+    document.getElementById('btn-img-6').addEventListener('click', function(){
+//        fetch('/api/user/2.json').then(function(response){
+//            response.text().then(function(text){
+//                document.getElementById('fetch-result').innerHTML = text;
+//            });
+//        });
+    });
+    document.getElementById('btn-7').addEventListener('click', function(){
+        fetch('/api/user/1.json').then(function(response){
+            response.text().then(function(text){
+                document.getElementById('test-5-iframe').innerHTML = text;
+            });
+        });
+    });
+    */
+    
+});
