aa93ae032fc6fa27b272d59086e8a395f1b9feb8
Felipe N. Moura <felipenmoura@gmail.com>
2016-12-06 12:07:55 -0200

    DSW init now respects indentation

--- bin/init.js
+++ bin/init.js
@@ -7,23 +7,23 @@ module.exports = (()=>{
             function treatIndex () {
                 const fd = fs.openSync(directory + '/index.html', 'a+');
                 const SW_CALL = `
-        <script src="dsw.js"></script>
-        <script>
-            DSW.setup()
-                .then(function(result){
-                    // tell your users about the good news
-                })
-                .catch(function(reason){
-                    // ops!
-                });
-        </script>`;
+@ind@ind<script src="dsw.js"></script>
+@ind@ind<script>
+@ind@ind@indDSW.setup()
+@ind@ind@ind@ind.then(function(result){
+@ind@ind@ind@ind@ind// tell your users about the good news
+@ind@ind@ind@ind})
+@ind@ind@ind@ind.catch(function(reason){
+@ind@ind@ind@ind@ind// ops!
+@ind@ind@ind@ind});
+@ind@ind</script>`;
                 const VIEWPORT = `
-        <meta name="viewport" content="width=device-width, initial-scale=1">`;
+@ind@ind<meta name="viewport" content="width=device-width, initial-scale=1">`;
                 const WP_META = `
-        <!-- WEB APP META DATA -->
-        <link rel="manifest" href="/webapp-manifest.json">
-        <meta name="theme-color" content="#DD5939"> <!-- set theme's color -->
-        <!-- /WEB APP META DATA -->`;
+@ind@ind<!-- WEB APP META DATA -->
+@ind@ind<link rel="manifest" href="/webapp-manifest.json">
+@ind@ind<meta name="theme-color" content="#DD5939"> <!-- set theme's color -->
+@ind@ind<!-- /WEB APP META DATA -->`;
                 const FULL_INDEX = `<!doctype html>
 <html>
     <head>
@@ -41,19 +41,27 @@ module.exports = (()=>{
                 if (!indexContent.replace(/[\n\r \t]/g, '').length) {
                     indexContent = FULL_INDEX;
                 } else {
+                    debugger;
+                    var indent = indexContent.match(/[\n|\r]([ \t]){2,}/g);
+                    if (indent) {
+                        indent = indent[0].replace(/[\n\r]/g, '');
+                    } else {
+                        indent = '  ';
+                    }
+
                     // if the index file doesn't have the viewport, we add it
                     if (!indexContent.match('\<meta(^\>)+name=[\'\"]viewport[\'\"]')) {
-                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${VIEWPORT}
+                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${VIEWPORT.replace(/@ind/g, indent)}
 $1</$2>`);
                     }
                     // if the index file doesn't have a DSW.setup call, we add it
                     if (!indexContent.match(/DSW([ \t\n\r]?).([ \t\n\r]?)setup([ \t\n\r]?)\(/)) {
-                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${SW_CALL}
+                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${SW_CALL.replace(/@ind/g, indent)}
 $1</$2>`);
                     }
                     // if the index file has no web app metadata, we add it
                     if (!indexContent.match('\<link(^\>)+rel=[\'\"]manifest[\'\"]')) {
-                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${WP_META}
+                        indexContent = indexContent.replace(/([ \t]+)?\<\/(head|body|html)\>/, `${WP_META.replace(/@ind/g, indent)}
 $1</$2>`);
                     }
                 }
